28558af1c10e4b3e517fdee9b6f596880a411b5b
Nitish <144581644+TensorTobio@users.noreply.github.com>
2025-07-21 21:51:31 +0400

    Update CodeChallenge1JSS.py

--- CodeChallenge1JSS.py
+++ CodeChallenge1JSS.py
@@ -1,3 +1,5 @@
+#1)
+
 rows = int(input("Enter the no. of rows : "))
 col = int(input("Enter the no. of columns : "))
 
@@ -31,3 +33,35 @@ for i in range(rows):
 
 if maxi != 0:
     print("The biggest palindrome is ", maxipal, "Length is ", maxi)
+
+
+
+#2)
+text = input("Enter the text to be encrypted : ")
+rows = int(input("Enter the no. of rows : "))
+x = len(text)
+encrypt = ""
+
+# For rows = 3
+for j in range(rows):
+    if (j + 1) % 2 == 0:
+        for i in range(j, x, rows - 1):
+            encrypt = encrypt + text[i]
+    else:
+        for i in range(j, x, rows + 1):
+            encrypt = encrypt + text[i]
+
+"""for k in encrypt[:5]:
+    print(k, end=" ")
+
+print()
+for k in encrypt[5:14]:
+    print(" ", k, end=" ")
+
+print()
+for k in encrypt[14:]:
+    print("   ", k, end=" ")
+print()"""
+
+print(f"Encrypted:  {encrypt}")
+print(f"Decrypted: {text}")
